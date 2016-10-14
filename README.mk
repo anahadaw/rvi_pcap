ff9dc287c3b34f19aa82272c7e781fd7fbb8b6e0
wadahana <wadahana@gmail.com>
2016-10-14 15:20:18 +0800

    modify readme

--- README.mk
+++ README.mk
@@ -1,5 +1,7 @@
 
 1. insert iphone or ipad, waiting for itunes found device ..
 
-2. double click rvi_cap.command or run rvi_cap.sh in console, save pcap file as YYYYMMDD_hhmmss.pcap in current path.
+2. double click rvi_cap.command or run rvi_cap.sh in console, to save pcap file as YYYYMMDD_hhmmss.pcap in current path.
+
+3. install xcode command line first.
 
