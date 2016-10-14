b49e649ec51a1fb35851b2cbe8ba91cd8615dc6b
wadahana <wadahana@gmail.com>
2016-10-14 15:12:05 +0800

    create readme

--- READMD.mk
+++
@@ -1,4 +0,0 @@
-
-1. 插上手机，等待itune弹出后
-2. 双击pcap.command运行, 生成YYYYMMDD_hhmmss.pcap时间格式的抓包文件
-3. 需要停止抓包时按"control+c" 活着直接关掉弹出的黑色窗口
---
+++ README.mk
@@ -0,0 +1,5 @@
+
+1. insert iphone or ipad, waiting for itunes found device ..
+
+2. double click rvi_cap.command or run rvi_cap.sh in console, save pcap file as YYYYMMDD_hhmmss.pcap in current path.
+
